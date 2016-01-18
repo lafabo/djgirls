adc6d39cb88bfc5cc4321118f5957c57d3af89a9
lafabo <lazyfatboy@ya.ru>
2016-01-19 02:05:47 +0300

    day 42

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+import datetime
+
 
 # Create your tests here.
+class QuestionMethodTests(TestCase):
+	def test_was_published_recently_with_future_question(self):
+		"""was_published_recently() should return false
+		when question pub_date > time.now"""
+		time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=time)
+		self.assertEqual(future_question.was_published_recently(), False)
--- polls/urls.py
+++ polls/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.polls, name='polls'),
-	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^$', views.PollsView.as_view(), name='polls'),
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
